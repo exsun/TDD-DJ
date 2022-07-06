603f2bbcec75eee41f3b2fa7183193208d406d67
ehsangh7 <[ehsanghochagh@gmail.com]>
2022-07-06 17:38:06 +0430

    pytest fixtures

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth.models import Group, Permission
+from account.models import User
+import pytest
 
-# Create your tests here.
+
+@pytest.fixture
+def user_A(db) -> Group:
+    group = Group.objects.create(name="app_users")
+    change_user_permission = Permission.objects.filter(
+        codename__in=["change_user", "view_user"],
+    )
+    group.permissions.add(*change_user_permission)
+    user = User.objects.create_user("A")
+    user.groups.add(group)
+    return user
+
+
+    
+def test_should_create_user_with_username(user_A: User) -> None:
+    assert user_A.username == "A"
+
+def test_should_check_password_for_user(user_A: User) -> None:
+    user_A.set_password("secret")
+    assert user_A.check_password("secret") is True
+
+def test_should_not_check_unusable_password(user_A: User) -> None:
+    user_A.set_password("secret")
+    user_A.set_unusable_password()
+    assert user_A.check_password("secret") is False
+
+def test_user_groups_exists(user_A: User) -> None:
+    assert user_A.groups.filter(name="app_users").exists()
